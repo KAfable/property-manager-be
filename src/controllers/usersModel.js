d309cbf65e676abbdf6d5f3f7d3a2be005838b3c
Anthony Vigliotta <anthonyvigz@gmail.com>
2019-11-08 15:18:38 -0600

    started user model to retrieve type of user

--- src/controllers/usersModel.js
+++ src/controllers/usersModel.js
@@ -1,9 +1,14 @@
 const db = require("../../database/db-config");
 
 module.exports = {
-  getAll
+  getAll,
+  findById
 };
 
 function getAll() {
   return db.from("users").select("*");
+}
+
+function findById(id) {
+    return db("users").select('username', 'type').where({ id });
 }
--- src/controllers/usersModel.test.js
+++ src/controllers/usersModel.test.js
@@ -95,3 +95,56 @@ it("should return all users", async () => {
     console.error(err);
     }
 })
+
+// test for finding a user and type by an ID
+
+it("should return a username and type by ID", async () => {
+
+    try {
+      await db.from("users").insert([{
+          id: 1,
+          username: "landlord",
+          password: "examplepass",
+          name: {
+            title: "Title",
+            firstname: "Firstname",
+            middlename: "Middlename",
+            lastname: "Lastname",
+            suffix: "Suffix",
+            preferredname: "Preferred"
+          },
+          email: "landlord@email.com",
+          address: {
+            street: "1 First St",
+            street2: "Suite 2",
+            city: "Salt Lake City",
+            state: "Utah",
+            zip: "84101",
+            country: "USA"
+          },
+          type: "landlord",
+          phone: "123-456-7890"
+        },
+        {
+          id: 2,
+          username: "dev",
+          password: "pass",
+          name: {
+            firstname: "Web",
+            lastname: "Dev"
+          },
+          email: "dev@email.com",
+          type: "dev"
+        }]);
+  
+      const user = await Users.findById(1);
+  
+      expect(user).toEqual({
+        username: "landlord",
+        type: "landlord"
+      });
+    } catch (err) {
+      console.error(err);
+      }
+  })
+  
